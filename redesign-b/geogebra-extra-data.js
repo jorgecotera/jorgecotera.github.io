9110cf2cfcd9316596a66a5ec78b92b4f73505ec
jorgecotera <160553384+jorgecotera@users.noreply.github.com>
2026-09-13 16:44:03 -0500

    Quitar material no perteneciente al repositorio GeoGebra

--- redesign-b/geogebra-extra-data.js
+++ redesign-b/geogebra-extra-data.js
@@ -23,6 +23,5 @@ window.geogebraExtraMaterials = [
   {id:"yQKY8pHN",title:"Recta y=ax+b",category:"Funciones lineales"},
   {id:"kbMFtWe4",title:"Función a Trozo 2",category:"Funciones"},
   {id:"BCCe35bj",title:"La Elipse con centro (h,k)",category:"Geometría analítica"},
-  {id:"VydFfksg",title:"Pendiente de una Recta a partir de Puntos",category:"Geometría analítica"},
-  {id:"B8xN4dX6",title:"Situaciones de variación lineal y cuadrática",category:"Modelación"}
+  {id:"VydFfksg",title:"Pendiente de una Recta a partir de Puntos",category:"Geometría analítica"}
 ];
